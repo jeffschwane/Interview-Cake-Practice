bee7bd8bd8fc73fc41a2cc82eba2b5c98ea6ba1c
Jeff Schwane <jschwane@gmail.com>
2021-04-01 16:16:49 -0400

    Cafe Order Problem

---
+++ cafe_order_checked.py
@@ -0,0 +1,76 @@
+import unittest
+
+"""My cake shop is so popular, I'm adding some tables and hiring wait staff so folks can have a cute sit-down cake-eating experience.
+
+I have two registers: one for take-out orders, and the other for the other folks eating inside the cafe. All the customer orders get combined into one list for the kitchen, where they should be handled first-come, first-served.
+
+Recently, some customers have been complaining that people who placed orders after them are getting their food first. Yikes—that's not good for business!
+
+To investigate their claims, one afternoon I sat behind the registers with my laptop and recorded:
+
+The take-out orders as they were entered into the system and given to the kitchen. (take_out_orders)
+The dine-in orders as they were entered into the system and given to the kitchen. (dine_in_orders)
+Each customer order (from either register) as it was finished by the kitchen. (served_orders)
+Given all three lists, write a function to check that my service is first-come, first-served. All food should come out in the same order customers requested it.
+"""
+
+def is_first_come_first_served(take_out_orders, dine_in_orders, served_orders):
+
+    # Check if we're serving orders first-come, first-served
+    # Check each order list to ensure that the order is maintained in the final list
+    
+    i_to = 0 # Take-out counter
+    i_di = 0 # Dine-in counter
+    
+    for order in served_orders:
+        if i_to < len(take_out_orders) and order == take_out_orders[i_to]:
+            i_to += 1
+        elif i_di < len(dine_in_orders) and order == dine_in_orders[i_di]:
+            i_di += 1
+        else:
+            return False
+    
+    # check for extra orders
+    if i_to == len(take_out_orders) and i_di == len(dine_in_orders):
+        return True 
+    else:
+        return False
+
+
+# Tests
+
+class Test(unittest.TestCase):
+
+    def test_both_registers_have_same_number_of_orders(self):
+        result = is_first_come_first_served([1, 4, 5], [2, 3, 6], [1, 2, 3, 4, 5, 6])
+        self.assertTrue(result)
+
+    def test_registers_have_different_lengths(self):
+        result = is_first_come_first_served([1, 5], [2, 3, 6], [1, 2, 6, 3, 5])
+        self.assertFalse(result)
+
+    def test_one_register_is_empty(self):
+        result = is_first_come_first_served([], [2, 3, 6], [2, 3, 6])
+        self.assertTrue(result)
+
+    def test_served_orders_is_missing_orders(self):
+        result = is_first_come_first_served([1, 5], [2, 3, 6], [1, 6, 3, 5])
+        self.assertFalse(result)
+
+    def test_served_orders_has_extra_orders(self):
+        result = is_first_come_first_served([1, 5], [2, 3, 6], [1, 2, 3, 5, 6, 8])
+        self.assertFalse(result)
+
+    def test_one_register_has_extra_orders(self):
+        result = is_first_come_first_served([1, 9], [7, 8], [1, 7, 8])
+        self.assertFalse(result)
+
+    def test_one_register_has_unserved_orders(self):
+        result = is_first_come_first_served([55, 9], [7, 8], [1, 7, 8, 9])
+        self.assertFalse(result)
+
+    def test_order_numbers_are_not_sequential(self):
+        result = is_first_come_first_served([27, 12, 18], [55, 31, 8], [55, 31, 8, 27, 12, 18])
+        self.assertTrue(result)
+
+unittest.main(verbosity=2)
